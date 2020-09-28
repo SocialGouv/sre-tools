b380896882c625c997085c7b5d0b36f607d81313
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-09-28 12:33:37 +0200

    fix(sre-tools): fix package.main

---
+++ packages/sre-seal/src/index.js
@@ -0,0 +1,9 @@
+const createSealedSecret = require("./createSealedSecret");
+const crypt = require("./crypt");
+const cryptFromSecrets = require("./cryptFromSecrets");
+
+module.exports = {
+  createSealedSecret,
+  crypt,
+  cryptFromSecrets,
+};
