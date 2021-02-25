3000ed36bb9c211bbcece8c417d05aa03d1bbbf9
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-02-25 11:32:24 +0100

    chore: fixes (#35)

--- packages/k8strip/src/replacers.js
+++ packages/k8strip/src/replacers.js
@@ -1,38 +1,53 @@
 const manifestReplacers = {
   Secret: (manifest) => ({
     ...manifest,
-    data: Object.keys(manifest.data).reduce(
-      (a, c) => ({ ...a, [c]: "some-secret-value" }),
-      {}
-    ),
+    data:
+      (manifest.data &&
+        Object.keys(manifest.data).reduce(
+          (a, c) => ({ ...a, [c]: "some-secret-value" }),
+          {}
+        )) ||
+      {},
   }),
   ConfigMap: (manifest) => ({
     ...manifest,
-    data: Object.keys(manifest.data).reduce(
-      (a, c) => ({ ...a, [c]: "some-configmap-value" }),
-      {}
-    ),
+    data:
+      (manifest.data &&
+        Object.keys(manifest.data).reduce(
+          (a, c) => ({ ...a, [c]: "some-configmap-value" }),
+          {}
+        )) ||
+      {},
   }),
   SealedSecret: (manifest) => ({
     ...manifest,
-    encryptedData: Object.keys(manifest.spec.encryptedData).reduce(
-      (a, c) => ({ ...a, [c]: "some-sealed-secret-value" }),
-      {}
-    ),
+    encryptedData:
+      (manifest.spec.encryptedData &&
+        Object.keys(manifest.spec.encryptedData).reduce(
+          (a, c) => ({ ...a, [c]: "some-sealed-secret-value" }),
+          {}
+        )) ||
+      {},
     spec: {
       ...manifest.spec,
-      encryptedData: Object.keys(manifest.spec.encryptedData).reduce(
-        (a, c) => ({
-          ...a,
-          [c]: "some-sealed-secret-value",
-        }),
-        {}
-      ),
+      encryptedData:
+        (manifest.spec.encryptedData &&
+          Object.keys(manifest.spec.encryptedData).reduce(
+            (a, c) => ({
+              ...a,
+              [c]: "some-sealed-secret-value",
+            }),
+            {}
+          )) ||
+        {},
     },
   }),
 };
 
 const replacers = (manifest) => {
+  if (!manifest.kind) {
+    return manifest;
+  }
   if (manifestReplacers[manifest.kind]) {
     return manifestReplacers[manifest.kind](manifest);
   }
