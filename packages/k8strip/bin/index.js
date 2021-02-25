40d4fed60fcec898d7b1877061b4570a7c5cd780
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-02-25 10:46:28 +0100

    chore: fixes

--- packages/k8strip/bin/index.js
+++ packages/k8strip/bin/index.js
@@ -1,24 +1,28 @@
 #!/usr/bin/env node
 
-const pipe = require("pipe-args").load();
+const getStdin = require("get-stdin");
 
 const k8strip = require("..");
 
 if (require.main === module) {
-  if (process.argv.length < 3) {
+  if (process.argv.length < 2) {
     console.error("Usage: cat sample.json | k8strip");
     throw new Error("k8strip need some JSON input");
   }
-  const input = process.argv[process.argv.length - 1];
-  try {
-    if (input) {
-      const data = JSON.parse(input);
-      console.log(JSON.stringify(k8strip(data), null, 2));
-    } else {
-      console.error("Usage: cat sample.json | k8strip");
+  (async () => {
+    const input = await getStdin();
+    try {
+      if (input) {
+        const data = JSON.parse(input);
+        console.log(JSON.stringify(k8strip(data), null, 2));
+      } else {
+        console.error("Usage: cat sample.json | k8strip");
+      }
+    } catch (e) {
+      console.error(
+        "Error: cannot parse input; k8strip only support JSON input"
+      );
+      console.error(e);
     }
-  } catch (e) {
-    console.error("Error: cannot parse input; k8strip only support JSON input");
-    console.error(e);
-  }
+  })();
 }
