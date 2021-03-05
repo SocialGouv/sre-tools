22960a7a49fe0e9a25bce1f18285642e850b6f79
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-05 11:22:58 +0100

    fix(webseal): dont double encrypt

--- packages/webseal/src/App.js
+++ packages/webseal/src/App.js
@@ -76,7 +76,7 @@ const Editor = () => {
           name: data.name,
           scope: data.scope,
           values: {
-            VALUE: value,
+            VALUE: data.value,
           },
         });
 
