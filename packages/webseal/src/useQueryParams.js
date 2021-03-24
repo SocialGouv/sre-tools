9e4dcb1ba929191fdfe8a8b919387073c55d6d53
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-25 00:35:33 +0100

    fix(webseal): fix weird issue

--- packages/webseal/src/useQueryParams.js
+++ packages/webseal/src/useQueryParams.js
@@ -2,14 +2,22 @@ import { parse, stringify } from "query-string";
 import { useCallback, useState } from "react";
 
 export const useQueryParams = () => {
- const urlParams = parse(window.location.search);
+  const urlParams = parse(window.location.search);
 
- const [innerData, setInnerData] = useState(urlParams);
+  const [innerData, setInnerData] = useState(urlParams);
 
- const setData = useCallback((data) => {
-     window.location.search = stringify(data);
-     setInnerData(data);
- },[setInnerData]);
+  const setData = useCallback(
+    (data) => {
+      // dont reload page
+      window.history.replaceState(
+        {},
+        window.title,
+        document.location.href.split("?")[0] + "?" + stringify(data)
+      );
+      setInnerData(data);
+    },
+    [setInnerData]
+  );
 
- return [innerData, setData];
-}
+  return [innerData, setData];
+};
