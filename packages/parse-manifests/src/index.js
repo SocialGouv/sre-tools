22c1a431eecc48d5f87574e626b7ecc685b2cdd8
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-09-16 01:38:10 +0200

    fix(parse-manifests): uniquify and sort docker images

--- packages/parse-manifests/src/index.js
+++ packages/parse-manifests/src/index.js
@@ -32,7 +32,7 @@ const getDeploymentAnnotation = (manifests, annotation) => {
 const getImages = (manifests) => {
   const deployments = manifests.filter((m) => m.kind === "Deployment");
   if (deployments.length) {
-    return deployments.flatMap((deploy) => [
+    const images = deployments.flatMap((deploy) => [
       ...deploy.spec.template.spec.containers.map(
         (container) => container.image
       ),
@@ -42,6 +42,7 @@ const getImages = (manifests) => {
         )) ||
         []),
     ]);
+    return Array.from(new Set(images)).sort();
   }
 };
 
