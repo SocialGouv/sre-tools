06cfbbce7dc7fc7b01d926511a439bfbe68524e4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-10-14 17:35:46 +0200

    feat(parse-manifests): add rancher deployments urls

--- packages/parse-manifests/__tests__/toMarkdown.test.js
+++ packages/parse-manifests/__tests__/toMarkdown.test.js
@@ -3,11 +3,20 @@ import toMarkdown from "../src/toMarkdown.js";
 import fs from "fs";
 import path from "path";
 
-//console.log(parseManifests(fs.readFileSync("../sample.yml").toString()));
+beforeEach(() => {
+  process.env.RANCHER_PROJECT_ID = "";
+});
 
 test("should render markdown", () => {
   const yaml = fs.readFileSync(path.join("sample.yml")).toString();
   const redirect = fs.readFileSync(path.join("redirect.yml")).toString();
+  expect(toMarkdown(yaml + redirect)).toMatchSnapshot();
+});
+
+test("should render markdown with rancher details", () => {
+  const yaml = fs.readFileSync(path.join("sample.yml")).toString();
+  const redirect = fs.readFileSync(path.join("redirect.yml")).toString();
+  process.env.RANCHER_PROJECT_ID = "xyz";
   expect(toMarkdown(yaml + redirect)).toMatchSnapshot();
 });
 
