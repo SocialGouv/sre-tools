82bf5b5f298fede18c0ae68534983e1b8d53e2a8
devthejo <jo@surikat.pro>
2022-06-27 09:32:58 +0200

    fix: decouple output

---
+++ packages/parse-manifests/src/summaryToMarkdown.js
@@ -0,0 +1,58 @@
+import {
+  getGrafanaLogsUrl,
+  getGrafanaWorkloadsUrl,
+  getRancherUrls,
+  getGrafanaPodsUrl,
+} from "./tools.js";
+
+const summaryToMarkdown = (parsed) => {
+
+  return `
+<details>
+  <summary>Ingresses</summary>
+
+${parsed.hosts.map((host) => ` - 🚀 [${host}](https://${host})`).join("\n")}
+
+</details>
+${
+  (parsed.redirects.length &&
+    `
+<details>
+  <summary>Redirects</summary>
+
+${parsed.redirects
+  .map(({ from, to }) => ` - https://${from} ➡️ ${to}`)
+  .join("\n")}
+
+</details>
+`) ||
+  ""
+}
+<details>
+  <summary>Docker images</summary>
+
+${parsed.images.map((image) => ` - 📦 docker pull ${image}`).join("\n")}
+
+</details>
+
+<details>
+  <summary>Debug</summary>
+
+ - [📕 Loki logs for namespace ${parsed.namespace}](${getGrafanaLogsUrl(
+    parsed
+  )})
+ - [📈 Pods monitoring for namespace ${parsed.namespace}](${getGrafanaPodsUrl(
+    parsed
+  )})
+ - [📈 Workloads monitoring for namespace ${
+   parsed.namespace
+ }](${getGrafanaWorkloadsUrl(parsed)})
+${getRancherUrls(parsed)
+  .map(({ name, url }) => ` - [👮‍♂️ ${name}](${url})`)
+  .join("\n")}
+
+</details>
+`;
+};
+
+export default summaryToMarkdown;
--- packages/parse-manifests/src/toMarkdown.js
+++ packages/parse-manifests/src/toMarkdown.js
@@ -1,60 +1,10 @@
 import parseManifests from "./index.js";
-import {
-  getGrafanaLogsUrl,
-  getGrafanaWorkloadsUrl,
-  getRancherUrls,
-  getGrafanaPodsUrl,
-} from "./tools.js";
+import summaryToMarkdown from "./summaryToMarkdown.js";
 
 const toMarkdown = (manifests) => {
   const parsed = parseManifests(manifests);
 
-  return `
-<details>
-  <summary>Ingresses</summary>
-
-${parsed.hosts.map((host) => ` - 🚀 [${host}](https://${host})`).join("\n")}
-
-</details>
-${
-  (parsed.redirects.length &&
-    `
-<details>
-  <summary>Redirects</summary>
-
-${parsed.redirects
-  .map(({ from, to }) => ` - https://${from} ➡️ ${to}`)
-  .join("\n")}
-
-</details>
-`) ||
-  ""
-}
-<details>
-  <summary>Docker images</summary>
-
-${parsed.images.map((image) => ` - 📦 docker pull ${image}`).join("\n")}
-
-</details>
-
-<details>
-  <summary>Debug</summary>
-
- - [📕 Loki logs for namespace ${parsed.namespace}](${getGrafanaLogsUrl(
-    parsed
-  )})
- - [📈 Pods monitoring for namespace ${parsed.namespace}](${getGrafanaPodsUrl(
-    parsed
-  )})
- - [📈 Workloads monitoring for namespace ${
-   parsed.namespace
- }](${getGrafanaWorkloadsUrl(parsed)})
-${getRancherUrls(parsed)
-  .map(({ name, url }) => ` - [👮‍♂️ ${name}](${url})`)
-  .join("\n")}
-
-</details>
-`;
+  return summaryToMarkdown(parsed)
 };
 
 export default toMarkdown;
